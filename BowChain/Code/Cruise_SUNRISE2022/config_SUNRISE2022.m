1ac13ca3d16adb40e28e5ec6512c91dbecc6e848
Jamie Hilditch <hilditch@stanford.edu>
2023-01-08 17:52:51 -0800

    started defining the SUNRISE2022 config

--- BowChain/Code/Cruise_SUNRISE2022/config_SUNRISE2022.m
+++ BowChain/Code/Cruise_SUNRISE2022/config_SUNRISE2022.m
@@ -10,100 +10,82 @@
 
 function config = config_SUNRISE2022()
 
-    %% Set some global default options for deployments
-    defaults = struct();
+    %% Set some global configurations
+    global_config = struct();
+    global_config.chain_model = 'cm_straight';
+    % global_config.time_offset_method = 'cohere';
+    % global_config.cohere_interval = [dunk_start_time, dunk_end_time];
+    global_config.raw2mat = false; % if true force reparse of data;
     
-    % This is the simplest model for initial processing -- just assume that the
-    % chain is vertical and compressing like a spring. Get the depth coordinate of
-    % each sensor by interpolating between pressure sensors.
-    defaults.chain_model = 'cm_straight';
-    
-    % We should do a "dunk" to calibrate sensor clocks eventually.
-    % defaults.time_offset_method = 'cohere';
-    % defaults.cohere_interval = [dunk_start_time, dunk_end_time];
-    
-    % Set this to true to force re-parsing of raw data even if .mat files already
-    % exist. Useful in case we find mistakes in parsing functions and need to
-    % correct them.
-    defaults.raw2mat = false;
-    
-    % Use the earliest and latest timestamp of all sensors if the start/end time
-    % aren't specified. Just in case we're missing a section_start_end_time.csv.
-    defaults.dn_range = [-inf inf];
-    
-    % Grid-averaging settings
-    % defaults.bin_method = 'average';
-    % defaults.bin_dt = 2;
-    % defaults.bin_dz = 1;
-    % defaults.bin_zlim = [-30 0];
-    
-    % Modify user_directories.m for your data location. For me this returns
-    % '/home/data/SUNRISE/Tchain', but everyone's local copy will probably be
-    % different.
-    dir_raw = fullfile(user_directories('SUNRISE2022'),'raw');
-    
+    % Get the tchain data directory
+    % Use an environment variable rather than user_directories.m
+    tchain_dir = getenv('SUNRISE2022_TCHAIN_DATA');
+    if isempty(tchain_dir); error("Environment variable 'SUNRISE2022_TCHAIN_DATA' not set"); end
+    if exist(tchain_dir) ~= 7; error("'SUNRISE2022_TCHAIN_DATA' is not a directory"); end
+
+
     %% Create deployment-specific configurations
     % This is where having a consistent file structure does 90% of the work for us!
-    % The expected format should look like this:
+    % The file structure should look like this:
     %
-    % └── Tchain
-    %     └── raw
-    %         └── Aries
+    % └── tchain_dir
+    %     └── Aries
+    %         └── raw  
     %             └── deploy_20210618
-    %                 ├── 060088_20210618_2140.rsk
-    %                 ├── 077416_20210618_1644.rsk
-    %                 ├── 077561_20210618_1649.rsk
-    %                 ├── 077565_20210618_1647.rsk
-    %                 ├── 077566_20210618_2148.rsk
-    %                 ├── 077568_20210618_2141.rsk
-    %                 ├── 101179_20210618_2145.rsk
-    %                 ├── 101180_20210618_2136.rsk
-    %                 ├── instrument_depths.csv
-    %                 ├── README.txt
-    %                 └── section_start_end_time.csv
+    %                 └── raw_rsk
+    %                 └── raw_mat (Dylan calls these files proc_mat but they're just parsed raw data)
+    %                 └── deployment_info.csv
+    %         └── processed_nc (where we save processed files - one netcdf per deployment)
+    %         └── sections (individual section files)
+    %     └── Pelican
+    %         └── raw
+    %         └── processed_nc
+    %         └── sections
+    %     └── PointSur
+    %         └── raw
+    %         └── processed_nc
+    %         └── sections
+    %     └── Polly
+    %         └── raw
+    %         └── processed_nc
+    %         └── sections
     
-    vessel_raw_folders = dir(fullfile(dir_raw));
-    vessel_names = setdiff({vessel_raw_folders.name},{'.','..'});
-    
+    vessel_names = ["Aries", "Pelican", "PointSur", "Polly"]
+  
     ndep = 0;
-    for v = 1:length(vessel_names)
-        deployments = dir(fullfile(dir_raw,vessel_names{v},'deploy*'));
+    for vessel = vessel_names
+        vessel_directory = fullfile(tchain_dir,vessel);
+        deployments = dir(fullfile(vessel_directory,raw,'deploy*'));
         for i = 1:length(deployments)
             ndep = ndep + 1;
             config(ndep).name = deployments(i).name;
-            config(ndep).vessel = vessel_names{v};
+            config(ndep).vessel = vessel;
+            
+            % TODO modify all this commented out code to read in sensor metadata
+            % % Read the sensors.csv file for instrument deployment positions
+            % t = readtable(fullfile(deployments(i).folder,deployments(i).name,'instrument_depths.csv'));
+            % config(ndep).sensor_sn = num2cell(t.serialnum);
+            % config(ndep).sensor_pos = t.depth_m_;
     
-            % Read the sensors.csv file for instrument deployment positions
-            t = readtable(fullfile(deployments(i).folder,deployments(i).name,'instrument_depths.csv'));
-            config(ndep).sensor_sn = num2cell(t.serialnum);
-            config(ndep).sensor_pos = t.depth_m_;
+            % % Try to read start & end time
+            % try
+            %     t = readtable(fullfile(deployments(i).folder,deployments(i).name,'section_start_end_time.csv'));
+            %     config(ndep).dn_range = datenum([t.start_time t.end_time]);
+            % catch err
+            %     % Default to full sensor time range if this fails
+            %     config(ndep).dn_range = [-inf inf];
+            % end
     
-            % Try to read start & end time
-            try
-                t = readtable(fullfile(deployments(i).folder,deployments(i).name,'section_start_end_time.csv'));
-                config(ndep).dn_range = datenum([t.start_time t.end_time]);
-            catch err
-                % Default to full sensor time range if this fails
-                config(ndep).dn_range = [-inf inf];
-            end
+            % Set raw_rsk data directory
+            config(ndep).dir_raw = fullfile(deployments(i).folder,deployments(i).name,raw_rsk);
     
-            % Set raw data directory
-            config(ndep).dir_raw = fullfile(deployments(i).folder,deployments(i).name);
-    
-            % Set processed data directory
-            config(ndep).dir_proc = strrep(config(ndep).dir_raw,'/raw/','/processed/');
+            % Set raw_mat data directory
+            config(ndep).dir_proc = fullfile(deployments(i).folder,deployments(i).name,raw_mat);
+
+            % Set the processed_nc file
+            config(ndep).nc_file = fullfile(vessel_directory,processed_nc,deployments(i).name);
         end
     end
-    config = fill_defaults(config,defaults);
-    
-    %% We can specify any deployment- or vessel-specific settings here
-    % There are a bunch of possible ways to do this, below is one example:
-    for i = 1:length(config)
-        switch config(i).vessel
-    
-          % ...
-        end
-    end
-    
+    config = fill_defaults(config,global_config);
     
     %% End of config_SUNRISE2022
