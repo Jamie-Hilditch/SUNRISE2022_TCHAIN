02d2342035c8844e4a150d2034f0526bd8a45f74
Jamie Hilditch <hilditch@stanford.edu>
2023-01-09 17:04:04 -0800

    RSKopen no longer returns dbid in parse_rbr_duet

--- BowChain/Code/ParseFunctions/parse_rbr_duet.m
+++ BowChain/Code/ParseFunctions/parse_rbr_duet.m
@@ -1,6 +1,7 @@
 function data = parse_rbr_duet(f_in)
     data = struct();
-    [rbr,dbid] = RSKopen(f_in);
+    %[rbr,dbid] = RSKopen(f_in);
+    rbr = RSKopen(f_in);
     tmp = RSKreaddata(rbr);
     data.dn = tmp.data.tstamp;
     data.t = tmp.data.values(:,1);
