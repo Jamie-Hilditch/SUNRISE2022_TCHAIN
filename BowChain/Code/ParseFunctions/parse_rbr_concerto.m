573708fcc9c14aad3b2f332755d1adfad29c2ad8
Jamie Hilditch <hilditch@stanford.edu>
2023-01-10 13:49:37 -0800

    concerto and duet now check channel

--- BowChain/Code/ParseFunctions/parse_rbr_concerto.m
+++ BowChain/Code/ParseFunctions/parse_rbr_concerto.m
@@ -2,9 +2,13 @@ function data = parse_rbr_concerto(f_in)
     data = struct();
     [rbr] = RSKopen(f_in);
     tmp = RSKreaddata(rbr);
+    [tmp,~] = RSKcorrecthold(tmp);
+    CT_lag = RSKcalculateCTlag(tmp);
+    tmp = RSKalignchannel(tmp, 'channel', 'Conductivity', 'lag', CT_lag);
+    tmp = RSKderivesalinity(tmp);
     data.dn = tmp.data.tstamp;
-    data.c = tmp.data.values(:,1);
-    data.t = tmp.data.values(:,2);
-    data.p = tmp.data.values(:,3);
-    data.s = gsw_SP_from_C(data.c,data.t,data.p);
+    data.c = tmp.data.values(:,getchannelindex(tmp, 'Conductivity'));
+    data.t = tmp.data.values(:,getchannelindex(tmp, 'Temperature'));
+    data.p = tmp.data.values(:,getchannelindex(tmp, 'Pressure'));
+    data.s = tmp.data.values(:,getchannelindex(tmp, 'Salinity'));
 end
--- BowChain/Code/ParseFunctions/parse_rbr_duet.m
+++ BowChain/Code/ParseFunctions/parse_rbr_duet.m
@@ -2,7 +2,8 @@ function data = parse_rbr_duet(f_in)
     data = struct();
     rbr = RSKopen(f_in);
     tmp = RSKreaddata(rbr);
+    [tmp,~] = RSKcorrecthold(tmp);
     data.dn = tmp.data.tstamp;
-    data.t = tmp.data.values(:,1);
-    data.p = tmp.data.values(:,2);
+    data.t = tmp.data.values(:,getchannelindex(tmp, 'Temperature'));
+    data.p = tmp.data.values(:,getchannelindex(tmp, 'Pressure'));
 end
