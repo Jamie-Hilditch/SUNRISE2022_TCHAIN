002cab5079aa67e6b0b25d63eebef65aaf2dbbe5
Jamie Hilditch <hilditch@stanford.edu>
2023-01-10 13:57:34 -0800

    correct hold on solos

--- BowChain/Code/ParseFunctions/parse_rbr_solo.m
+++ BowChain/Code/ParseFunctions/parse_rbr_solo.m
@@ -2,6 +2,7 @@ function data = parse_rbr_solo(f_in)
     data = struct();
     rbr = RSKopen(f_in);
     solo = RSKreaddata(rbr);
+    [tmp,~] = RSKcorrecthold(tmp);
     data.dn = solo.data.tstamp;
     data.t = solo.data.values;
     % include code to add salinity to structure here
